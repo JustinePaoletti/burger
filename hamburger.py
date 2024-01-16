aa5174387ebc569c55b8ef5a8884a996be9fcca4
Dorian Brousse <dorianbrousse@sfr.fr>
2024-01-16 16:09:41 +0100

    plusieurs lignes indépendantes

--- hamburger.py
+++ hamburger.py
@@ -1,29 +1,59 @@
 import flet as ft 
+
+class Item:
+    def __init__(self, text, price):
+        self.text = text
+        self.price = price 
+
+burger = Item(text = "🍔", price = 5.95)
+fries = Item(text = "🍟", price = 3.60)
+salad = Item(text = "🥗", price = 8.30)
+drink = Item(text = "🍹", price = 2.60)
+
 def main(page: ft.Page):
-    page.title = "Flet counter example"
+    page.title = "I Can Has Cheezburger ?"
+    page.window_height = 430
+    page.window_width = 400
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    def row(item):
 
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
+        txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
 
-    page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
+        def minus_click(e): 
+            if txt_number.value == "0":
+                page.update()
+            else :
+                txt_number.value = str(int(txt_number.value) - 1) 
+                page.update()
+               
+
+        def plus_click(e):
+            txt_number.value = str(int(txt_number.value) + 1)
+            page.update()
+        
+        
+        page.add(
+            ft.Row(
+                [
+                    ft.Text(item.text, size = 25),
+                    ft.Text(item.price, size = 20),
+                    txt_number,
+                    ft.IconButton(ft.icons.ADD, on_click=plus_click),
+                    ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
         )
-    )
+
+        
+    
+    row(burger)
+    row(fries)
+    row(salad)
+    row(drink)
+ 
+
 
 ft.app(target=main)
-
-print('hello')
