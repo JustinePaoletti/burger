58bcca86b23a1797e25f4e857e22af62572d90cb
Dorian Brousse <dorianbrousse@sfr.fr>
2024-01-16 16:57:06 +0100

    allez

---
+++ product.py
@@ -0,0 +1,32 @@
+import flet as ft
+
+def Product(manger, prix, page):
+
+        txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+
+        def minus_click(e): 
+            if txt_number.value == "0":
+                page.update()
+            else :
+                txt_number.value = str(int(txt_number.value) - 1) 
+                page.update()
+               
+
+        def plus_click(e):
+            txt_number.value = str(int(txt_number.value) + 1)
+            page.update()
+        
+        
+        
+        return(ft.Row(
+            [
+                ft.Text(manger, size = 25),
+                ft.Text(prix, size = 20),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+        )
